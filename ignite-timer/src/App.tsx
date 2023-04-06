ceeb8e048b2f7c89f9aa084239f9b87441d7e075
thayna <arraisthayna2106@gmail.com>
2023-04-06 16:39:48 -0300

    studying about styled-components

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/button/Button";
+
 export function App() {
   return(
     <div className="App">
-      <h1>Sss</h1>
+      <Button variant={'primary'}></Button>
+      <Button variant={'secondary'}></Button>
+      <Button variant={'danger'}></Button>
+      <Button variant={'success'}></Button>
     </div>
   )
 }
---
+++ ignite-timer/src/components/button/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonContainer, ButtonVariant } from "./Button.styles";
+
+interface ButtonProps {
+  variant: ButtonVariant;
+}
+
+export function Button({variant = 'primary'}:ButtonProps) {
+  return (
+    <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+  )
+}
