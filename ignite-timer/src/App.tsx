2cc1fdba50eb9e3813da1f9ec7611c764c9653fb
thayna <arraisthayna2106@gmail.com>
2023-04-08 19:37:22 -0300

    react router dom

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/button/Button'
+import { BrowserRouter } from 'react-router-dom'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant={'primary'}></Button>
-      <Button variant={'secondary'}></Button>
-      <Button variant={'danger'}></Button>
-      <Button variant={'success'}></Button>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
