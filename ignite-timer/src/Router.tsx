9ed8564180e16db44e70b5a976f11223846c32ea
thayna <arraisthayna2106@gmail.com>
2023-04-09 18:22:00 -0300

    header and layout

--- ignite-timer/src/Router.tsx
+++ ignite-timer/src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
-import { DefaultLayout } from './layouts/DefaultLayout'
+import { DefaultLayout } from './layouts/DefaulLayout'
 
 export function Router() {
   return (
