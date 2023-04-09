9a0104bdb4daf6630cb9dabcc5bcf0875f1f820f
thayna <arraisthayna2106@gmail.com>
2023-04-09 19:05:23 -0300

    home page

---
+++ ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Play } from 'phosphor-react'
+import { CountdownContainer, FormContainer, HomeContainer, Separator } from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
